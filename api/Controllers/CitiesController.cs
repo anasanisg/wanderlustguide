cec4f0cb8730157f02818f71ca12c7b8a5c9f8fc
Anas Anis <75489042+anasanisg@users.noreply.github.com>
2023-10-22 03:27:46 +0200

    2.0.0 - second Deployment

---
+++ api/Controllers/CitiesController.cs
@@ -0,0 +1,20 @@
+﻿using api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+
+[ApiController]
+[Route("api/[Controller]")]
+public class CitiesController : ControllerBase
+{
+
+    public static readonly CityData[] CityDataArray = new CityData[]
+    {
+  new CityData(1,"Dortmund","","Hallo Dortmund",600000,500),
+  new CityData(2,"Köln","","Hallo Köln"),
+    };
+
+    [HttpGet]
+    public ActionResult<IEnumerable<CityData>> GetCities() => CityDataArray.ToList<CityData>();
+
+}
---
+++ api/Models/CityData.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace api.Models;
+
+public class CityData
+{
+    public required int cityId { get; set; }
+
+    public required string name { get; set; }
+
+    public required string cityImage { get; set; }
+
+    public string? cityDescription { get; set; }
+
+    public int? population { get; set; }
+
+    public int? rate { get; set; }
+
+    [SetsRequiredMembers]
+    public CityData(int cityId, string name, string cityImage, string? cityDescription = null, int? population = null, int? rate = null)
+    {
+        this.cityId = cityId;
+        this.name = name;
+        this.cityImage = cityImage;
+        this.cityDescription = cityDescription;
+        this.population = population;
+        this.rate = rate;
+
+    }
+}
